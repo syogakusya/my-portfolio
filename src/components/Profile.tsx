5297454667f44f0b508a1f5a86c11b8b5a4be540
syogakusya <Chitsuoman404@outlook.jp>
2024-09-23 03:40:04 +0900

    ある程度できた

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-
+import Link from "next/link";
 interface ProfileProps {
   imageUrl: string;
   name: string;
@@ -10,19 +10,27 @@ interface ProfileProps {
 const Profile: React.FC<ProfileProps> = ({ imageUrl, name, bio }) => {
   return (
     <div className="w-full border-b-4 border-black p-24">
-      <div className="flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg p-6 max-w-3xl mx-auto">
+      <div className="flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg p-6 max-w-7xl mx-auto">
         <div className="mb-6 md:mb-0 md:mr-8">
           <Image
             src={imageUrl}
             alt={name}
-            width={1000}
-            height={1000}
-            className="rounded-full"
+            width={600}
+            height={600}
+            className="rounded-full mb-4"
           />
+          <div className="flex justify-center">
+            <Link
+              className="border border-gray-50 rounded-sm px-3 py-2 text-center shadow-lg font-bold"
+              href="https://twitter.com/tsuchida_z"
+            >
+              X(Twitter)
+            </Link>
+          </div>
         </div>
         <div className="text-center md:text-left">
           <h2 className="text-3xl font-bold mb-4">{name}</h2>
-          <p className="text-gray-600 text-lg">{bio}</p>
+          <p className="text-gray-700 text-lg">{bio}</p>
         </div>
       </div>
     </div>
