7cb3a358c1bccd4728e952e9ede925dadcb3a028
syogakusya <ChitsuoMan404@outlook.jp>
2024-09-22 14:32:09 +0900

    hello world!! and add Header.tsx

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-xl font-bold">
+          My Portfolio
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/">ホーム</Link>
+          </li>
+          <li>
+            <Link href="/about">自己紹介</Link>
+          </li>
+          <li>
+            <Link href="/projects">プロジェクト</Link>
+          </li>
+          <li>
+            <Link href="/contact">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
