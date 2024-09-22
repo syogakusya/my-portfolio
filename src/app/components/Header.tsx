dddd7582d099871252a194f113b714b9d66dbaea
syogakusya <ChitsuoMan404@outlook.jp>
2024-09-22 14:41:28 +0900

    名前ができた

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-white text-gray-800 p-4 border-b border-black">
       <nav className="container mx-auto flex justify-between items-center">
-        <Link href="/" className="text-xl font-bold">
-          最強つちだズ
+        <Link href="/" className="text-xl font-bold flex flex-col items-center">
+          <div className="flex justify-between w-full">
+            <span className="text-xs text-gray-500">Artisan</span>
+            <span className="text-xs text-gray-500">SaikyoTsuchidaZ</span>
+          </div>
+          <span className="text-4xl">最強つちだズ</span>
         </Link>
         <ul className="flex space-x-4">
           <li>
