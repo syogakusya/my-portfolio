2699b9cb1b9f8daab9e6c92ee2cc2b53a937a756
syogakusya <ChitsuoMan404@outlook.jp>
2024-09-23 14:37:23 +0900

    作品一覧でけた！

---
+++ src/app/works/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { works } from "@/data/works";
+import Image from "next/image";
+
+export async function generateStaticParams() {
+  return works.map((work) => ({
+    id: work.id,
+  }));
+}
+
+const WorkDetail = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  const work = works.find((work) => work.id === id);
+
+  if (!work) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/2 p-8 overflow-y-auto">
+        <h1 className="text-4xl font-bold mb-4">{work.title}</h1>
+        <p className="text-lg mb-4">{work.longDescription}</p>
+        <a href={work.githubUrl} className="text-blue-500 hover:underline">
+          GitHub
+        </a>
+      </div>
+      <div className="w-1/2 p-8 overflow-y-auto">
+        {work.imageUrls && (
+          <Image
+            src={`/${work.imageUrls[0]}`}
+            alt={work.title}
+            width={200}
+            height={200}
+            className="w-full h-auto object-cover"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default WorkDetail;
--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,4 +1,38 @@
+import React from "react";
+import { works } from "@/data/works";
+import Link from "next/link";
+import Image from "next/image";
+
 const Works = () => {
-  return <div className="text-center text-4xl font-bold m-8">準備中</div>;
+  return (
+    <div className="p-8">
+      <h1 className="text-4xl font-bold text-center mb-8">作品一覧</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {works.map((work) => (
+          <div
+            key={work.id}
+            className="border rounded-lg shadow-lg overflow-hidden"
+          >
+            <Link href={`/works/${work.id}`}>
+              {work.imageUrls && (
+                <Image
+                  src={`/${work.imageUrls[0]}`}
+                  alt={work.title}
+                  width={200}
+                  height={200}
+                  className="w-full h-48 object-cover"
+                />
+              )}
+              <div className="p-4">
+                <h2 className="text-2xl font-bold">{work.title}</h2>
+                <p className="text-sm">{work.shortDescription}</p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
+
 export default Works;
