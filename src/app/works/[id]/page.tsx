ae9c6a2e14e81a0055e7d639e40e42c1544fa350
syogakusya <Chitsuoman404@outlook.jp>
2024-09-27 13:18:01 +0900

    [id]/page.tsx レスポンシブ

--- src/app/works/[id]/page.tsx
+++ src/app/works/[id]/page.tsx
@@ -16,22 +16,22 @@ const WorkDetail = ({ params }: { params: { id: string } }) => {
   }
 
   return (
-    <div className="flex h-screen">
-      <div className="w-1/2 p-8 overflow-y-auto border-r-4 border-black">
-        <h1 className="text-4xl font-bold mb-4">{work.title}</h1>
-        <p className="text-lg mb-4">{work.longDescription}</p>
-        <a href={work.githubUrl} className="text-blue-500 hover:underline">
+    <div className="flex flex-col md:flex-row min-h-screen">
+      <div className="w-full md:w-1/2 p-4 md:p-8 overflow-y-auto border-b-4 md:border-b-0 md:border-r-4 border-black">
+        <h1 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">{work.title}</h1>
+        <p className="text-base md:text-lg mb-2 md:mb-4">{work.longDescription}</p>
+        <a href={work.githubUrl} className="text-blue-500 hover:underline text-sm md:text-base">
           GitHub
         </a>
       </div>
-      <div className="w-1/2 p-8 overflow-y-auto">
+      <div className="w-full md:w-1/2 p-4 md:p-8 overflow-y-auto">
         {work.imageUrls && (
           <Image
             src={`/${work.imageUrls[0]}`}
             alt={work.title}
-            layout="responsive"
             width={700}
             height={475}
+            layout="responsive"
             className="object-cover"
           />
         )}
